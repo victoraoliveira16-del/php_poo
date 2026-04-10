9f99fe63c56f22efa38a9b2277adf75eb10a9448
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-04-10 10:56:09 -0300

    exemplos

---
+++ exemplo09.php
@@ -0,0 +1,24 @@
+<?php
+
+class Filme
+{
+    public $nome;
+    public $saldo;
+
+    function __construct($nome, $saldo)
+    {
+        $this->nome = $nome;
+        $this->saldo = $saldo;
+    }
+
+    function incrementarSaldo($valor)
+    {
+        $this->saldo = $this->saldo + $valor;
+    }
+}
+
+$filme = new Filme("Toy Story", 10);
+$filme->incrementarSaldo(5);
+
+echo "Nome do Filme: $filme->nome<br>";
+echo "Saldo disponóivel: $filme->saldo";
