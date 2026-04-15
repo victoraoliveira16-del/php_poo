ed45d6db34b0e85df1c292ae8ee16879c2792771
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-04-15 11:21:27 -0300

    protected e private

--- exemplo17.php
+++ exemplo17.php
@@ -12,9 +12,30 @@ class Visibilidade
         $this->varProtected = $varProtected;
         $this->varPrivate = $varPrivate;
     }
+
+    public function publicFunc()
+    {
+        echo "Método Público<br>";
+    }
+
+    protected function protectedFunc()
+    {
+        echo "Método Protegido<br>";
+    }
+
+    private function privateFunc()
+    {
+        echo "Método Privado<br>";
+    }
 }
 
 $teste = new Visibilidade(1, 2, 3);
 echo "Atributo Public = $teste->varPublic";
-echo "Atributo Protected = $teste->varPotected;
-echo "Atributo Private = $teste->varPrivate";
+// echo "Atributo Protected = $teste->varPotected;
+// echo "Atributo Private = $teste->varPrivate";
+
+echo "<br>";
+
+$teste->publicFunc();
+$teste->protectedFunc();
+$teste->privateFunc();
