28c80abdde537377985e932ff3b68dbe46359c19
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-04-17 08:27:08 -0300

    atividade

--- Carro.php
+++ Carro.php
@@ -1,36 +1,37 @@
 <?php
-class Carro {
+class Carro
+{
     // 🚩 RISCO: Atributo público permite valores ilegais
     public $modelo;
     private $velocidade;
 
-    public function __construct($modelo, $velocidade) {
+    public function __construct($modelo, $velocidade)
+    {
         $this->modelo = $modelo;
         $this->velocidade = $velocidade;
     }
 
-    public function getVelocidade(){
+    public function getVelocidade()
+    {
         return $this->velocidade;
     }
-    public function setVelocidade($novaVelocidade){
+    public function setVelocidade($novaVelocidade)
+    {
         if ($novaVelocidade < 0) {
-            echo "Velocidade não pode ser negativa!<br>";
-        } elseif ($novaVelocidade > 200) {
-            echo "Velocidade muito alta!<br>";
-        } else {
-            $this->velocidade = $novaVelocidade;
+            echo "Parecendo uma tartaruga!<br>";
+        } else if ($novaVelocidade > 200) {
+            echo "Diminua quer morrer!<br>";
         }
+        $this->velocidade = $novaVelocidade;
     }
-
 }
 
 // --- TESTE DO VEÍCULO ---
 $meuCarro = new Carro("Senai-Mobile", 0);
 
 // O desastre: alteração direta sem validação
-$meuCarro->getVelocidade() = 5000; // Velocidade de foguete?
-$meuCarro->getVelocidade ()= -60;   // Carro andando no tempo?
+$meuCarro->setVelocidade(5000); // Velocidade de foguete?
+$meuCarro->setVelocidade(200);   // Carro andando no tempo?
 
 echo "Modelo: " . $meuCarro->modelo . "<br>";
-echo "Velocidade atual: " . $meuCarro->velocidade . " km/h";
-?>
+echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
