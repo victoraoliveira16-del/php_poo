d41df3a07efb355c224a51ee6cdbcdb82fc73cbe
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-04-29 16:44:10 -0300

    finalizando o exemplo 20

--- exemplo20.php
+++ exemplo20.php
@@ -31,3 +31,27 @@ class NotificadorWhatsapp implements INotificador
         echo "Whatsapp enviado para {$destinatario}. Mensagem: {$mensagem}.";
     }
 }
+
+// Classe que usa a interface 
+class SistemaDeNotificacoes
+{
+    private $notificador;
+
+    public function __construct(INotificador $notificador)
+    {
+        $this->notificador = $notificador;
+    }
+
+    public function notificarUsuario($destinatario, $mensagem)
+    {
+        $this->notificador->enviar($destinatario, $mensagem);
+    }
+}
+
+$sistemaEmail = new SistemaDeNotificacoes(new NoticadorEmail());
+$sistemaSMS = new SistemaDeNotificacoes(new NotificadorSMS());
+$sistemaWhatsapp = new SistemaDeNotificacoes(new NotificadorWhatsapp());
+
+$sistemaEmail->notificarUsuario("joao@email.com", "Seu Pedido foi confirmado");
+$sistemaSMS->notificarUsuario("17981256230", "Seu Pedido foi confirmado");
+$sistemaWhatsapp->notificarUsuario("17991293329", "Seu Pedido foi confirmado");
